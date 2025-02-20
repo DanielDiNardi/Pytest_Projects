3194c1753aec97aa2007c48d81f2ae0fc1407949
Daniel Di Nardi <dan.dinardi@hotmail.com>
2025-02-20 14:52:28 +0000

    Finished Pytest Tutorial bar AI testing

---
+++ tests/test_my_functions.py
@@ -0,0 +1,34 @@
+
+import time
+import pytest
+import source.my_functions as my_functions
+
+def test_add():
+	result = my_functions.add(1, 4)
+	assert result == 5
+
+def test_add_strings():
+	result = my_functions.add("I like", " cheese")
+	assert result == "I like cheese"
+
+def test_divide():
+	result = my_functions.divide(10, 5)
+	assert result == 2
+
+def test_divide_by_zero():
+	with pytest.raises(ValueError):
+		result = my_functions.divide(10, 0)
+
+@pytest.mark.slow
+def test_very_slow():
+	time.sleep(5)
+	result = my_functions.divide(10, 5)
+	assert result == 2
+
+@pytest.mark.skip(reason="This feature is currently broken")
+def test_add():
+	assert myfunctions.add(1, 2) == 3
+
+@pytest.mark.xfail(reason="We know we cannot divide by zero")
+def test_divide_by_zero_broken():
+	my_functions.divide(4, 0)
